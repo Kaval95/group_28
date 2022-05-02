b58cabc12b4765000d8a5ae5cd4fea31ba32a2be
Vladyslav Kovalchuk <100120137+VladyslavKoval@users.noreply.github.com>
2022-05-02 22:09:14 +0300

    Update JS HW

--- JavaScript/JS_Home_Work_1.js
+++ JavaScript/JS_Home_Work_1.js
@@ -98,9 +98,12 @@ if(age_1<age_2) {
 }
 Вывести в консоль результат работы функции с возрастами 17, 18, 61
 
+let age_2 = 18;
+let age_3 = 60
+
 function chekcAge(age){
     if(age < age_2) {
-        console.log("You dont have access cause your age is " + age_1 + ". Its less then")
+        console.log("You dont have access cause your age is " + age + ". Its less then")
     } else if(age >= age_2 && age < age_3) {
         console.log("Welcome")  
     } else if(age > age_3) {
@@ -114,3 +117,31 @@ function chekcAge(age){
 chekcAge(17)
 chekcAge(18)
 chekcAge(61)
+
+2*:
+Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных. И если он не Number - кидалась ошибка.
+
+let age_2 = 18
+let age_3 = 60
+
+function chekcAge(age){
+    
+    if (typeof age === "number") {
+          if (age < age_2) {
+            console.log("You dont have access cause your age is " + age + ". Its less then")
+        } else if(age >= age_2 && age < age_3) {
+            console.log("Welcome")  
+        } else if(age > age_3) {
+            console.log("Keep calm and look Culture channel")
+        } else {
+            console.log("Technical work")
+        }
+
+    }
+   else{
+       console.log("Error! Value age = " + typeof age + " please write number value")
+   } 
+
+}
+
+chekcAge(81)
